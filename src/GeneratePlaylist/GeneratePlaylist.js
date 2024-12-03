b05eaada71ef4b4178a61bed1f78dfcbe77b67c8
Samuel Zhao <samdraw123@gmail.com>
2024-12-03 08:54:23 -0500

    added taskbar

--- src/GeneratePlaylist/GeneratePlaylist.js
+++ src/GeneratePlaylist/GeneratePlaylist.js
@@ -178,28 +178,44 @@ const GeneratePlaylist = () => {
   };
 
   return (
-    <><div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link text-white active" href="/">
-          Home
-        </a>
-        <a
-          className="nav-item nav-link text-white"
-          href="/generateplaylist"
-        >
-          Generate A Playlist
-        </a>
-        <a className="nav-item nav-link text-white" href="/login">
-          Login
-        </a>
-        <a className="nav-item nav-link text-white" href="/signup">
-          Signup
-        </a>
-        <a className="nav-item nav-link text-white" href="/howtouse">
-          How To Use
-        </a>
+    <><nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <a className="navbar-brand text-white" href="/public">
+        Spotify Playlist Maker
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link text-white active" href="/">
+            Home
+          </a>
+          <a
+            className="nav-item nav-link text-white"
+            href="/generateplaylist"
+          >
+            Generate A Playlist
+          </a>
+          <a className="nav-item nav-link text-white" href="/login">
+            Login
+          </a>
+          <a className="nav-item nav-link text-white" href="/signup">
+            Signup
+          </a>
+          <a className="nav-item nav-link text-white" href="/howtouse">
+            How To Use
+          </a>
+        </div>
       </div>
-    </div><div className="container mt-5">
+    </nav><div className="container mt-5">
         <h1>Generate Playlist</h1>
         <button className="btn btn-primary mb-4" onClick={handleLogin}>
           Login With Spotify
