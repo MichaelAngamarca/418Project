2ee3e03ee4f68d950043ec3dd2d67320db5c2916
Samuel Zhao <samdraw123@gmail.com>
2024-12-09 17:27:08 -0500

    so I can pull

--- src/HomePage/Homepage.js
+++ src/HomePage/Homepage.js
@@ -32,7 +32,7 @@ const Homepage = () => {
       </div>
 
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <a className="navbar-brand text-white" href="/public">
+        <a className="navbar-brand text-white " href="/public">
           Spotify Playlist Maker
         </a>
         <button
